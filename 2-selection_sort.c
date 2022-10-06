952a06a71113e9331a160a308edc5ad9d9f9fc93
Ousmane Konate <konatealim@gmail.com>
2022-10-06 18:27:39 +0200

    add

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting algorithm
+ * @array: array to sort
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, min, j, temp;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min])
+            {
+                min = j;
+            }
+        }
+        temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+        if (i != min)
+            print_array(array, size);
+    }
+}
