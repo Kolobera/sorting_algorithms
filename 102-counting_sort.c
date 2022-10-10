a5d407a7f5cc8f38b3eee0b7aa6b67f135d12691
Ousmane Konate <konatealim@gmail.com>
2022-10-10 21:28:28 +0200

    add

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,7 +7,7 @@
  */
 void counting_sort(int *array, size_t size)
 {
-    int i, k, n = size;
+    int i, j, l, m, k, n = size;
     int *b, *count;
 
     for (i = 0; array[i]; i++)
@@ -20,16 +20,16 @@ void counting_sort(int *array, size_t size)
         return;
     for (i = 0; i < n; i++)
         ++b[array[i]];
-    for (i = 1; i <= k; i++)
-        b[i] += b[i - 1];
+    for (j = 1; j <= k; j++)
+        b[j] += b[j - 1];
     print_array(b, ((size_t)k + 1));
     count = malloc(sizeof(int) * (n));
     if (!count)
         return;
-    for ( i = n - 1; i >= 0; i--)
-        count[--b[array[i]]] = array[i];
-    for (i = 0; i < n; i++)
-        array[i] = count[i];
+    for (l = n - 1; l >= 0; l--)
+        count[--b[array[l]]] = array[l];
+    for (m = 0; m < n; m++)
+        array[m] = count[m];
     free(b);
     free(count);
 }
--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -8,16 +8,16 @@
  */
 void countsort(int *A, int n, int pos)
 {
-    int count[10] = {0},b[10],  i;
+    int count[10] = {0},b[10], i, j, k, l;
 
     for (i = 0; i < n; i++)
         ++count[(A[i]/pos)%10];
-    for (i = 1; i <= 10; i++)
-        count[i] += count[i - 1];
-    for (i = n - 1; i >= 0; i--)
-        b[--count[(A[i]/pos)%10]] = A[i];
-    for (i = 0; i < n; i++)
-        A[i] = b[i];
+    for (j = 1; j <= 10; j++)
+        count[j] += count[j - 1];
+    for (k = n - 1; k >= 0; k--)
+        b[--count[(A[k]/pos)%10]] = A[k];
+    for (l = 0; l < n; l++)
+        A[l] = b[l];
 }
 /**
  * radix_sort - radix sort algorithm
@@ -26,16 +26,16 @@ void countsort(int *A, int n, int pos)
  */
 void radix_sort(int *array, size_t size)
 {
-    int i, k, n = size;
+    int i, j, k, n = size;
 
     for (i = 0; array[i]; i++)
     {
         if (array[i] > k)
             k = array[i];
     }
-    for (i = 1; k/i > 0; i = i * 10)
+    for (j = 1; k/j > 0; j = j * 10)
     {
-        countsort(array, n, i);
+        countsort(array, n, j);
         print_array(array, size);
     }
 }
