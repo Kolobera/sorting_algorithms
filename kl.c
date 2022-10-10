154ff31b5a9859d46ac24dcb29424752a4dbd213
Ousmane Konate <konatealim@gmail.com>
2022-10-10 21:14:43 +0200

    add

---
+++ kl.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+
+void countsort(int *A, int n, int pos)
+{
+    int count[10] = {0},b[10],  i;
+
+    for (i = 0; i < n; i++)
+        ++count[(A[i]/pos)%10];
+    for (i = 1; i <= 10; i++)
+        count[i] += count[i - 1];
+    for (i = n - 1; i >= 0; i--)
+        b[--count[(A[i]/pos)%10]] = A[i];
+    for (i = 0; i < n; i++)
+        A[i] = b[i];
+}
+/**
+ * radix_sort - radix sort algorithm
+ * @array: array to sort
+ * @size: size of array 
+ */
+void radix_sort(int *array, size_t size)
+{
+    int i, k, n = size;
+
+    for (i = 0; array[i]; i++)
+    {
+        if (array[i] > k)
+            k = array[i];
+    }
+    for (i = 1; k/i > 0; i = i * 10)
+    {
+        countsort(array, n, i);
+        print_array(array, size);
+    }
+}
