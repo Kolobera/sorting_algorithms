cd1c932d51ba8c4edca2c1aa306414c463085f44
Kolo <konatealim@gmail.com>
2022-10-10 12:37:46 -0700

    add

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,35 +1,44 @@
 #include "sort.h"
 
 /**
- * counting_sort - counting sort algorithm
+ * counting_sort - sorts an array with the Counting sort algorithm
  * @array: array to sort
- * @size: size of array 
+ * @size: size of the array
  */
 void counting_sort(int *array, size_t size)
 {
-    int i, j, l, m, k, n = size;
-    int *b, *count;
+	int *count_arr, *out_arr, max, num, j, l;
+	size_t i, k, m, n;
 
-    for (i = 0; array[i]; i++)
-    {
-        if (array[i] > k)
-            k = array[i];
-    }
-    b = malloc(sizeof(int) * (k + 1));
-    if (!b)
-        return;
-    for (i = 0; i < n; i++)
-        ++b[array[i]];
-    for (j = 1; j <= k; j++)
-        b[j] += b[j - 1];
-    print_array(b, ((size_t)k + 1));
-    count = malloc(sizeof(int) * (n));
-    if (!count)
-        return;
-    for (l = n - 1; l >= 0; l--)
-        count[--b[array[l]]] = array[l];
-    for (m = 0; m < n; m++)
-        array[m] = count[m];
-    free(b);
-    free(count);
+	if (size < 2)
+		return;
+
+	max = array[0];
+	for (i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	count_arr = malloc(sizeof(size_t) * (max + 1));
+	out_arr = malloc(sizeof(int) * size);
+
+	for (j = 0; j <= max; j++)
+		count_arr[j] = 0;
+	for (k = 0; k < size; k++)
+	{
+		num = array[k];
+		count_arr[num] += 1;
+	}
+	for (l = 1; l <= max; l++)
+		count_arr[l] += count_arr[l - 1];
+	print_array(count_arr, max + 1);
+	for (m = 0; m < size; m++)
+	{
+		out_arr[count_arr[array[m]] - 1] = array[m];
+		count_arr[array[m]]--;
+	}
+	for (n = 0; n < size; n++)
+		array[n] = out_arr[n];
+
+	free(count_arr);
+	free(out_arr);
 }
